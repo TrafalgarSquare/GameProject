7b89ef75c2c82814d3d15508f63988ff8e252ff6
HristoVutov <vutov23@gmail.com>
2015-01-30 13:07:37 +0200

    Adding files

---
+++ src/Bullet.java
@@ -0,0 +1,34 @@
+
+public class Bullet {
+   private int posX;
+   private int posY;
+   
+public int getPosX() {
+	return posX;
+}
+public void setPosX(int posX) {
+	this.posX = posX;
+}
+public int getPosY() {
+	return posY;
+}
+public void setPosY(int posY) {
+	this.posY = posY;
+}
+
+public Bullet(){
+	
+}
+   
+public Bullet(int posX,int posY){
+	this.posX=posX;
+	this.posX=posY;	
+}
+   
+	
+	
+	
+	
+	
+
+}
